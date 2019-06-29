2ac6db560dc6a032732cf08dee0afac3a72d38c4
kennCK <kennettecanales@gmail.com>
2019-06-29 12:25:42 +0800

    contact

--- increment/src/components/contents/Contact.vue
+++ increment/src/components/contents/Contact.vue
@@ -2,18 +2,23 @@
   <div class="increment-wrapper">
     <h1 class="text-center" style="margin-top: 50px;">Contact Us</h1>
     <div class="platform-container">
+      <label v-if="validationFlag === true" class="text-danger">Opps! All fields are required.</label>
+      <br>
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Full Name"/>
+        <label class="text-danger" v-if="contact.full_name !== null && contact.full_name.length < 5">Full name must be greater than 5 characters.</label>
+        <input type="text" class="form-control" placeholder="Full Name" v-model="contact.full_name" v-bind:class="{'error-border': contact.full_name !== null && contact.full_name.length < 5, 'sucess-border': contact.full_name !== null && contact.full_name.length >= 5}"/>
       </div>
       <div class="form-group">
-        <input type="email" class="form-control" placeholder="Email"/>
+        <label class="text-danger" v-if="emailErrorFlag === true">Invalid email address.</label>
+        <input type="email" class="form-control" v-bind:class="{'error-border': emailErrorFlag === true, 'sucess-border': emailErrorFlag === false}" placeholder="Email" v-on:input="emailInput" :value="contact.email"/>
       </div>
       <div class="form-group">
-        <textarea rows="7" placeholder="Projects details" class="form-control"></textarea>
+        <label class="text-danger" v-if="contact.details !== null && contact.details.length < 5">Details must be greater than 50 characters</label>
+        <textarea rows="7" placeholder="Projects details" class="form-control" v-model="contact.details" v-bind:class="{'error-border': contact.details !== null && contact.details.length < 50, 'sucess-border': contact.details !== null && contact.details.length >= 50}"></textarea>
       </div>
     </div>
     <div class="text-center buttons">
-      <div class="btn btn-primary">SEND</div>
+      <div class="btn btn-primary" @click="submit()">SEND</div>
     </div>
   </div>
 </template>
@@ -72,6 +77,12 @@
 .form-control{
   min-height: 50px !important;
 }
+.error-border{
+  border: solid 1px $error;
+}
+.sucess-border{
+  border: solid 1px $secondary;
+}
 @media (max-width: 992px) {
   .platform-container, .rectangle{
     width: 96%;
@@ -85,6 +96,42 @@
 export default {
   data () {
     return {
+      emailErrorFlag: null,
+      contact: {
+        full_name: null,
+        email: null,
+        details: null
+      },
+      validationFlag: false
+    }
+  },
+  methods: {
+    emailInput(event){
+      this.contact.email = event.target.value
+      this.validateEmail()
+    },
+    validateEmail(){
+      let reg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+.[a-zA-Z0-9]*$/
+      if(reg.test(this.contact.email) === false){
+        this.emailErrorFlag = true
+        return false
+      }else{
+        this.emailErrorFlag = false
+        return true
+      }
+    },
+    submit(){
+      if(this.validateEmail() === false){
+        this.validationFlag = true
+      }
+      if((this.contact.full_name !== null && this.contact.full_name.length < 5) || this.contact.full_name === null){
+        this.validationFlag = true
+      }
+      if((this.contact.details !== null && this.contact.details.length < 5) || this.contact.details === null){
+        this.validationFlag = true
+      }else{
+        this.validationFlag = false
+      }
     }
   }
 }
